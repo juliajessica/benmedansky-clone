ac0aa535afdeef049bb3cce60eea48a483277e09
julia sheremet <juliajessicasher@gmail.com>
2018-05-25 10:53:27 -0700

    link js sheet to project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+// $(document).ready(function() {
+//
+//   function hide(clickedX) {
+//     document.getElementById(clickedX).style.display = 'none';
+//   }
+// });
